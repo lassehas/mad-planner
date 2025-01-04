96ef6b71367b4182ed454f12c71f57c4573fc092
lassehas <haslundlasse@icloud.com>
2025-01-04 21:20:12 +0100

    removed hasVerifiedEmail

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,6 +73,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@mad-planner.dk') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@mad-planner.dk');
     }
 }
