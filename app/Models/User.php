557f5060d8d9274279a4b970aa1eb02689d89987
lassehas <haslundlasse@icloud.com>
2025-01-04 21:19:03 +0100

    Made it possible to access admin panel with correct email

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
+use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -67,5 +69,10 @@ class User extends Authenticatable
             return $this->household_in->first();
         }
         return null;
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@mad-planner.dk') && $this->hasVerifiedEmail();
     }
 }
