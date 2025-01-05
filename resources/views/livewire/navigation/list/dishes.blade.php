031c60e5e990302c4b36bd9d6e68101adb61a65e
lassehas <haslundlasse@icloud.com>
2025-01-05 15:02:11 +0100

    restyled lists views to table

--- resources/views/livewire/navigation/list/dishes.blade.php
+++ resources/views/livewire/navigation/list/dishes.blade.php
@@ -1,13 +1,33 @@
 <div>
-    @foreach ($dishes as $dish)
-        <div>
-            <div>
-                {{ $dish->name }} ({{ $dish->price }} kr.)
-                @if (auth()->user()->find_suiteable_household() != null)
-                    <button wire:click="add_to_buy_list({{ $dish->id }})">Tilføj til indkøbsliste</button>
-                @endif
-                <button wire:click="edit_dish({{ $dish->id }})">Ændre</button>
-            </div>
-        </div>
-    @endforeach
+    <table style="width: 100%; border-collapse: collapse;">
+        <thead>
+            <tr>
+                <th style="text-align: left; width: 60%;">Navn</th>
+                <th style="text-align: left; width: 25%;">Pris</th>
+                <th style="text-align: right; width: 15%;"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($dishes as $dish)
+                <tr>
+                    <td>{{ $dish->name }}</td>
+                    <td>{{ $dish->price }} kr.</td>
+                    <td style="text-align: right;">
+                        @if (auth()->user()->find_suiteable_household() != null)
+                            <button wire:click="add_to_buy_list({{ $dish->id }})" style="margin-right: 8px;">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" style="width: 20px; height: 20px;">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                                </svg>
+                            </button>
+                        @endif
+                        <button wire:click="edit_dish({{ $dish->id }})">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" style="width: 20px; height: 20px;">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                            </svg>
+                        </button>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
