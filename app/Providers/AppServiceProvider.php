51784f99793d6a95835bfc196a64756b76fe57ef
lassehas <haslundlasse@icloud.com>
2025-01-10 09:42:11 +0100

    added notification positioning maybe

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Livewire\Notifications;
+use Filament\Support\Enums\VerticalAlignment;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Notifications::verticalAlignment(VerticalAlignment::Start);
     }
 
     /**
